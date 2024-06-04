4831fdfd41035f099fcf91434a99061b90632b1b
hashimxkhan <khan521@purdue.edu>
2024-06-04 16:55:23 +0500

    Added fronted Created frontend for the calculator following a specific theme for design

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let firstNum
+let secondNum
+let operator
 function add(a, b) {
     return a + b
 }
@@ -16,3 +19,32 @@ function mod(a, b) {
 function power(a, b) {
     return a ** b
 }
+
+function operate(firstNum, secondNum, operator) {
+    switch(operator) {
+        case "+":
+            add(firstNum, secondNum)
+            break
+        case "-":
+            subtract(firstNum, secondNum)
+        case "x":
+            multiply(firstNum, secondNum)
+        case "/":
+            divide(firstNum, secondNum)
+        case "%":
+            mod(firstNum, secondNum)
+        case "^":
+            power(firstNum, secondNum)
+    }
+}
+
+const display = document.querySelector(".display")
+display.textContent = "000"
+/*
+const buttons = document.querySelector(".buttons")
+for (let i = 0; i <= 9; i++) {
+const digit = document.createElement("button")
+digit.textContent = i
+digit.className = "btn"
+buttons.appendChild(digit)
+} */
