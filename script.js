dea1f31edb946c3f2230e2e1678afe9130e14465
hashimxkhan <khan521@purdue.edu>
2024-06-13 14:01:31 +0500

    Added extra functionality Implemented modulus and exponential functions

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let firstNum
 let secondNum
 let operator
-let counter = 0
+let ans
 function add(a, b) {
     return a + b
 }
@@ -24,42 +24,61 @@ function power(a, b) {
 function operate(firstNum, secondNum, operator) {
     switch(operator) {
         case "+":
-            add(firstNum, secondNum)
+          ans =  add(firstNum, secondNum)
             break
         case "-":
-            subtract(firstNum, secondNum)
+          ans =  subtract(firstNum, secondNum)
+            break
         case "x":
-            multiply(firstNum, secondNum)
+          ans = multiply(firstNum, secondNum)
+            break
         case "/":
-            divide(firstNum, secondNum)
+           ans = divide(firstNum, secondNum)
+            break
         case "%":
-            mod(firstNum, secondNum)
+           ans = mod(firstNum, secondNum)
+            break
         case "^":
-            power(firstNum, secondNum)
+          ans = power(firstNum, secondNum)
+            break
     }
+    if (ans > 1e6 || ans < 1e-6) {
+        ans = ans.toExponential(2)
+    }
+    return ans
 }
-
-
 const display = document.querySelector(".display")
 const digits = document.querySelectorAll(".digit")
 digits.forEach(element => { 
     element.addEventListener("click", function(event) {
-        let cur = display.textContent
-        display.textContent = cur +event.target.textContent
+        let cur = event.target.textContent
+        
+        if (operator === undefined) {
+            firstNum = (firstNum === undefined) ? cur : firstNum + cur
+            firstNum = Number(firstNum)
+            display.textContent = firstNum
+        } else {
+            secondNum = (secondNum === undefined) ? cur : secondNum + cur
+            secondNum = Number(secondNum)
+            display.textContent = secondNum
+        }
     })
 });
 const operators = document.querySelectorAll(".operator")
 operators.forEach(element => {
     element.addEventListener("click", function(event) {
-        element.style.backgroundColor = "gray"
+        if (firstNum != undefined) {
         operator = event.target.textContent
-        firstNum = display.textContent
+        }
+        secondNum = undefined
     })
     
 });
 const ac = document.querySelector(".ac") 
 ac.addEventListener("click", function(){
     display.textContent = ""
+    firstNum = undefined
+    secondNum = undefined
 })
 
 const del = document.querySelector(".del") 
@@ -67,12 +86,18 @@ del.addEventListener("click", function(){
     let content = display.textContent.substring(0, display.textContent.length - 1)
     display.textContent = content
 })
-
-/*
-const buttons = document.querySelector(".buttons")
-for (let i = 0; i <= 9; i++) {
-const digit = document.createElement("button")
-digit.textContent = i
-digit.className = "btn"
-buttons.appendChild(digit)
-} */
+const equals = document.querySelector(".equals")
+equals.addEventListener("click", function() {
+   if (firstNum != undefined && secondNum != undefined) {
+    firstNum = Number(firstNum)
+    secondNum = Number(secondNum)
+    display.textContent = operate(firstNum, secondNum, operator)
+    console.log(firstNum)
+    console.log(operator)
+    console.log(secondNum)
+    console.log(operate(firstNum,secondNum,operator))
+    console.log(typeof(operator))
+    secondNum = undefined
+    firstNum = ans
+   }
+})
